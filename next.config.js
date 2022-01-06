9a3dc21d6df3eface459c2a72fabd8b518523197
Bryan Burnett <crankyhippo@gmail.com>
2022-01-06 12:22:58 -0800

    Update

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    customKey: 'https://api-us-west-2.graphcms.com/v2/ckvsh166b133t01xk0fzgbeyp/master',
+  },
+  images: {
+    domains: ['media.graphcms.com'],
+  },
+};
