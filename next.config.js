da4a4942044c3b108b92eb48e44b22bf77ed9d7f
Bryan Burnett <crankyhippo@gmail.com>
2022-01-06 19:09:27 -0800

    Remove file update

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    customKey: 'https://api-us-west-2.graphcms.com/v2/ckvsh166b133t01xk0fzgbeyp/master',
-  },
-  images: {
-    domains: ['media.graphcms.com'],
-  },
-};
